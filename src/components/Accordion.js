6f75cfbb1cdf7c43c4fd732456ae10492d5d6624
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-10 16:47:02 -0400

    added

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -4,13 +4,13 @@ const Accordion = ({ items }) => {
   const renderedItems = items.map(item => {
     return (
         <React.Fragment key={item.title}>
-       <div className="title active">
-       <i className="dropdown icon"></i>
-       {item.title}
-     </div>
-     <div className="content active">
-        <p>{item.content}</p>
-     </div>
+         <div className="title active">
+           <i className="dropdown icon"></i>
+           {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+       </div>
     </React.Fragment>
     );
   });
