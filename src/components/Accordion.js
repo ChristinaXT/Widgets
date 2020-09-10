7876b78f63cdb6974f8f827d60a798e286eb98a0
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-10 16:51:41 -0400

    onclick

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -4,7 +4,12 @@ const Accordion = ({ items }) => {
   const renderedItems = items.map(item => {
     return (
         <React.Fragment key={item.title}>
-         <div className="title active">
+         <div
+           className="title active"
+           onClick={() => console.log('Title clicked')}
+           >
+
+
            <i className="dropdown icon"></i>
            {item.title}
         </div>
