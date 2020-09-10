59d5bed280223b6b03282aba9a04ac3752faff38
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-10 12:48:32 -0400

    added title

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Accordion = ({ items }) => {
   const renderedItems = items.map(item => {
-    
+    return <div>
+       <div className="title active">
+    </div>
   });
 
   return <h1>{items.length}</h1>;
