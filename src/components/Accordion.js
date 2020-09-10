01d24093ff790bca5637779800d9ac2964380c72
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-10 16:40:07 -0400

    added fragment

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Accordion = ({ items }) => {
   const renderedItems = items.map(item => {
-    return <div key={item.title}>
+    return <React.Fragment key={item.title}>
        <div className="title active">
        <i className="dropdown icon"></i>
        {item.title}
