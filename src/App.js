c882db1e6c250302c71db95b7e14d52cda587af1
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-10 12:01:30 -0400

    added another

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Accordion from './components/Accordion';
 
 const items = [
   {
-    title: 'What is React?'
+    title: 'What is React?',
     content: 'React is a front end JavaScript framework'
   },
   {
-    title: 'Why use React?'
+    title: 'Why use React?',
     content: 'React is a favorite JS Library among engineers'
   },
   {
-
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
   }
-]
+];
 
 export default () => {
   return (
