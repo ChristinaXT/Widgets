7ec80369ba7d41cc0ba57dfbbb5a5f7b2f247f8a
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-10 11:57:52 -0400

    added array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 
+const items = [
+  {
+    title: 'What is React?'
+    content: 'React is a front end JavaScript framework'
+  }
+]
+
 export default () => {
   return (
     <div>
