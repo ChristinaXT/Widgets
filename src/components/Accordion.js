d47324e31e7f133260e01e5d8a77d3f4c36eca17
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-15 10:58:39 -0400

    empty string

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -8,7 +8,7 @@ const Accordion = ({ items }) => {
   };
 
   const renderedItems = items.map((item, index) => {
-    const active = index === activeIndex ? 'active'
+    const active = index === activeIndex ? 'active' : '';
 
     return (
         <React.Fragment key={item.title}>
