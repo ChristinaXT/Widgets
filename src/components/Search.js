4ef4e22ffcc3517a82bd8f03d8ec5aeec16e10f0
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-18 16:42:38 -0400

    added programming

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
   const [results, setResults] = useState([]);
 
 
