a359793a015742a4255da7922ae9d1a939687503
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-18 15:04:17 -0400

    added

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,10 +4,8 @@ import axios from 'axios';
 const Search = () => {
   const [term, setTerm] = useState('');
 
-  console.log('I RUN WITH EVERY RENDER')
 //everytime you use useEffect, you are going to use an empty array, an array with a value inside (one or more elements) of it or nothing at all
      useEffect(() => {
-       console.log('I RUN AFTER EVERY RENDER AND AT INITIAL RENDER');
      } [term]);
 
   return (
