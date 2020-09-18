0b38ee14df64cb306129bca1d1fa7084be48852d
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-18 14:23:04 -0400

    fixed

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 const Search = () => {
   const [term, setTerm] = useState('');
 
-  console.log('I RUND WITH EVERY RENDER')
+  console.log('I RUN WITH EVERY RENDER')
 //everytime you use useEffect, you are going to use an empty array, an array with a value inside (one or more elements) of it or nothing at all
      useEffect(() => {
        console.log('I RUN AFTER EVERY RENDER');
