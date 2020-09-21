19d575c7b44f9eab7ab6e5ff83d892a4b68e9be6
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-21 16:47:35 -0400

    pageid

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,7 +27,7 @@ const Search = () => {
 
      const renderedResults = results.map((result) => {
         return (
-          <div key={result} className="item">
+          <div key={result.pageid} className="item">
             <div className="content">
              <div className="header">
                {result.title}
