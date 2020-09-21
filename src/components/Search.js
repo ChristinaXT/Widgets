d94fd81e02dbbe25ab5133f787f17a9dffe4a0e0
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-21 18:09:32 -0400

    comment

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,30 +5,30 @@ const Search = () => {
   const [term, setTerm] = useState('programming');
   const [results, setResults] = useState([]);
 
-//everytime you use useEffect, you are going to use an empty array, an array with a value inside (one or more elements) of it or nothing at all
-     // useEffect(() => {
-     //   const search = async () => {
-     //     const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-     //       params: {
-     //         action: 'query',
-     //         list: 'search' ,
-     //         origin: '*',
-     //         format: 'json',
-     //         srsearch: term,
-     //       },
-     //     });
-     //
-     //     setResults(data.query.search);
-     //   };
-     //
-     //   const timeoutId = setTimeout(() => {
-     //     if (term) {
-     //       search();
-     //     }
-     //   }, 500);
-     // }, [term]);
+everytime you use useEffect, you are going to use an empty array, an array with a value inside (one or more elements) of it or nothing at all
+     useEffect(() => {
+       const search = async () => {
+         const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+           params: {
+             action: 'query',
+             list: 'search' ,
+             origin: '*',
+             format: 'json',
+             srsearch: term,
+           },
+         });
 
+         setResults(data.query.search);
+       };
      
+       const timeoutId = setTimeout(() => {
+         if (term) {
+           search();
+         }
+       }, 500);
+     }, [term]);
+
+
 
      const renderedResults = results.map((result) => {
         return (
