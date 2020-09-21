498f1bc6203708087c8b286b099ef4120a995270
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-21 17:21:32 -0400

    space

--- src/components/Search.js
+++ src/components/Search.js
@@ -29,7 +29,12 @@ const Search = () => {
         return (
           <div key={result.pageid} className="item">
             <div className="right floated content">
-              <a className="ui button">Go</a>
+              <a
+               className="ui button"
+                
+               >
+               Go
+               </a>
             </div>
             <div className="content">
              <div className="header">{result.title}</div>
