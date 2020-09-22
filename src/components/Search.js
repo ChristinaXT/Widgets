6a13a99c3b3a484ff3760eea266dc93a03f1c42c
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-22 17:29:56 -0400

    delete

--- src/components/Search.js
+++ src/components/Search.js
@@ -33,23 +33,7 @@ const Search = () => {
      search();
   }, [debouncedTerm]);
 
-     useEffect(() => {
-
-
-      if (term && !results.length) {
-        search();
-      } else {
-        const timeoutId = setTimeout(() => {
-          if (term) {
-            search();
-          }
-        }, 1000);
-
-        return () => {
-          clearTimeout(timeoutId);
-        };
-      }
-    }, [term, results.length]);
+    
 
 
 
