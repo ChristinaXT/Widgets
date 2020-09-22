7153e5b26ca0907e4bffda21459f90de439fa33f
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-22 17:26:06 -0400

    edit

--- src/components/Search.js
+++ src/components/Search.js
@@ -17,24 +17,24 @@ const Search = () => {
   }, [term]);
 
   useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search' ,
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
 
+      setResults(data.query.search);
+    };
 
   }, [debouncedTerm]);
 
      useEffect(() => {
-       const search = async () => {
-         const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-           params: {
-             action: 'query',
-             list: 'search' ,
-             origin: '*',
-             format: 'json',
-             srsearch: term,
-           },
-         });
 
-         setResults(data.query.search);
-       };
 
       if (term && !results.length) {
         search();
