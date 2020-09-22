f5ae4d11fa1ce316d26b7a9dfca987c25f655927
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-22 17:10:35 -0400

    set

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('programming');
-  comst [debouncedTerm]
+  comst [debouncedTerm, setDebouncedTerm]
   const [results, setResults] = useState([]);
 
      useEffect(() => {
