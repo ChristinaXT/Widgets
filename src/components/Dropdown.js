042ebe503a9a5dc64cc3814f7789cc67f4201a4e
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-28 18:48:41 -0400

    added select

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -15,7 +15,7 @@ const Dropdown = ({ options }) => {
          <label className="label">Select a Color</label>
          <div className="ui selection dropdown visible active">
            <i className="dropdown icon"></i>
-           <div className="text"></div>
+           <div className="text">Select Color</div>
         </div>
        </div>
      </div>
