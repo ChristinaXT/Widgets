8cd7d1b3f47debda86ac6cdff3650cd659b0afec
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-29 15:50:22 -0400

    space

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const renderedOptions = options.map((option) => {
     return (
-      <div key={option.value} className="item">
+      <div
+        key={option.value}
+        className="item">
+
+
          {option.label}
         </div>
     );
