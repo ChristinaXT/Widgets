1fa42e3abbfc5933847793c657a24a33a45505f3
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-29 15:48:48 -0400

    selected label

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -15,7 +15,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
          <label className="label">Select a Color</label>
          <div className="ui selection dropdown visible active">
            <i className="dropdown icon"></i>
-           <div className="text">Select Color</div>
+           <div className="text">{selected.label}</div>
            <div className="menu visible transition">
              {renderedOptions}
            </div>
