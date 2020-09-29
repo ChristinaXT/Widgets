669ba65c662bb997d7d796336fdbc159e114310b
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-29 16:40:04 -0400

    added syntax

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -25,7 +25,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
          <label className="label">Select a Color</label>
          <div
          onClick={() => setOpen(!open)}
-         className={`ui selection dropdown visible active`}
+         className={`ui selection dropdown ${visible active}`}
          >
            <i className="dropdown icon"></i>
            <div className="text">{selected.label}</div>
