d463a4ea94f61bab6dcb1c154ab2b23acb6e4a57
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-29 17:01:54 -0400

    added use effect funtion

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from 'react';
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
 
+  useEffect(() => {
+
+  }, );
+
   const renderedOptions = options.map((option) => {
      if (option.value === selected.value) {
        return null;
