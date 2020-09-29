4ed1dc178505befb7259fd25a61f981cab90e0df
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-29 17:00:35 -0400

    useeffect hook

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
