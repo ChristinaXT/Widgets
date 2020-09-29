622625edd51be1201a1654bebdd5f494bd280f26
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-29 16:21:02 -0400

    usestate hook

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
-  
+
   const renderedOptions = options.map((option) => {
      if (option.value === selected.value) {
        return null;
