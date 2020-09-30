dfce7b44759e3ddf22fcf99e5f55be922b2ea71e
Christina Williams <christinaxtwilliams@gmail.com>
2020-09-30 16:33:25 -0400

    log

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,7 +5,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 
   useEffect(() => {
     document.body.addEventListener('click',(event) => {
-
+      console.log(event.target);
      setOpen(false);
     });
   }, []);
