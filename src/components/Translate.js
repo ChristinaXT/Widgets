32b30f03679a1db574d324f665bc6a5c4ac901c8
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-09 13:30:28 -0500

    added info

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
 
+const options = [
+  {
+    label: 'Afrikaans',
+    value: 'af'
+  },
+  
+];
+
+
 const Translate = () => {
   return (
     <div>
