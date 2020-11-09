eb22fd49e6220b3abefb0e8a32e65e6fbfcd8a5e
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-09 13:27:42 -0500

    added div

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import Dropdown from './Dropdown';
 
 const Translate = () => {
-
+  return (
+    <div>
+     <Dropdown />
+    </div>
+  );
 };
 
 export default Translate;
