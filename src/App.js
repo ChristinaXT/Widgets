d959287fb46a7af1fff019b19a2d6ff809fc3b93
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-09 13:45:55 -0500

    added import

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
   {
