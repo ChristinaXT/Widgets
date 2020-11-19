5d75f8a37da09d8b8cc4da27ee0e8911d3383101
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-19 16:50:00 -0500

    adding another useEffect

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -5,7 +5,9 @@ const Convert = ({ language, text }) => {
    const [translated, setTranslated] = useState('');
    const [debouncedText, setDebouncedText] = useState(text);
 
-   
+   useEffect(() => {
+     
+   });
 
   useEffect(() => {
     const doTranslation = async () => {
