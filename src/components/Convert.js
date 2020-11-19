26de96cc36e9541542ccc9a7463912ac3f0cd9d5
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-19 15:52:08 -0500

    added translated

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
-   const 
+   const [translated, setTranslated] = useState('')
 
   useEffect(() => {
     axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
