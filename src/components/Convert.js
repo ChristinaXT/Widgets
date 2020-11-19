8418cb67240982e41709e7f989c1324fde55336a
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-19 15:52:35 -0500

    punct

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
-   const [translated, setTranslated] = useState('')
+   const [translated, setTranslated] = useState('');
 
   useEffect(() => {
     axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
