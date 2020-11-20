f75eb04bd4701f17316559b137d4d04cc1c39fc9
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-20 18:59:48 -0500

    added

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -8,7 +8,6 @@ const Convert = ({ language, text }) => {
    useEffect(() => {
      const timerId = setTimeout(() => {
         setDebouncedText(text);
-        
      }, 500);
 
      return () => {
