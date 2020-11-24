a55ceec8f8fd1a7d473061aa98be6d93958a1ce7
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-24 16:05:21 -0500

    remove set up

--- src/components/Route.js
+++ src/components/Route.js
@@ -4,7 +4,15 @@ import { useState } from 'react';
 
 const Route = ({ path, children }) => {
     useEffect(() => {
+      const onLocationChange = () => {
+        console.log('Location Change');
+      };
+
       window.addEventListener('popstate' onLocationChange);
+
+      return () => {
+        window.removeEventListener('popstate', onLocationChange);
+      };
     }, []);
 
    return window.location.pathname === path
