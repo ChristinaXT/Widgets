64b1c084a731a1baf45ab889e94a75d330a921d5
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-24 15:56:34 -0500

    added popstate logic

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,6 +4,9 @@ const Link = ({ className, href, children }) => {
   const onClick = (event) => {
     event.prevent.default();
     window.history.pushState({}, '', href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
