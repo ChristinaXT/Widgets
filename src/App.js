1f2b57b31291573889b46fc8cba83e23ed850ab5
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-24 15:10:48 -0500

    header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
 import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
