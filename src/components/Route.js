c17c136c359906c4244004054b4947ea4381e5a7
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-24 16:00:35 -0500

    added window event

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,7 +1,12 @@
 // import React from 'react';
+import { useState } from 'react';
 
 
 const Route = ({ path, children }) => {
+    useEffect(() => {
+      window.addEventListener('');
+    }, []);
+
    return window.location.pathname === path
     ? children
     : null;
