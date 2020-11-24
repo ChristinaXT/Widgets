e913b2c99e0adb1e2c8e802ae585f274967996cc
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-24 16:19:33 -0500

    added

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
-    if (event.metaKey || event.ctrlKey){
+    if (event.metaKey || event.ctrlKey) {
       return;
     }
-    
+
     event.prevent.default();
     window.history.pushState({}, '', href);
 
