03d0b2a8eb8c34a6e9e8c5a9198d0c602a349e2b
Christina Williams <christinaxtwilliams@gmail.com>
2020-11-24 15:42:26 -0500

    added classifications

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Link = () => {
-  return <a>Link</a>;
+const Link = ({ className, href, children }) => {
+  return (
+    <a className={className} href={href}>
+     {children}
+   </a>
+  ); 
 };
 
 export default Link;
