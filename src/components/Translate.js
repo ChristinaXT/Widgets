224b79dc910f7ff451671857ebff7f04fff09e54
Christina Williams <christinaxtwilliams@gmail.com>
2020-12-10 12:14:32 -0500

    added style

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -125,7 +125,7 @@ const Translate = () => {
 
   return (
     <div>
-      <div className="ui form">
+      <div className="ui form trans-form">
         <div className="field">
           <label>Enter Text</label>
          <input value={text} onChange={(e) => setText(e.target.value)} />
@@ -136,9 +136,11 @@ const Translate = () => {
        selected={language}
        onSelectedChange={setLanguage}
        options={options}
+
        />
+
      <hr />
-     <h3 className="ui header">Output</h3>
+     <h3 className="ui header output">Output</h3>
      <Convert text={text} language={language} />
     </div>
   );
